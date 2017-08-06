ca3d3f6eb94839a6621a7666af925ae562d638de
gracka92 <gracjana.leonowicz@gmail.com>
2017-08-06 19:05:19 +0200

    first commit

--- TableViewsss/ViewController.swift
+++ TableViewsss/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBAction func sliderChanged(_ sender: Any) {
+        tableView.reloadData()
+    }
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((Int(indexPath.row) + 1) * Int(slider.value * 20))
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
